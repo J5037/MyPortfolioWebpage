6fc34bd5bc816a0d7b05d3cf272fb8139ce78cda
J5037 <89667630+J5037@users.noreply.github.com>
2024-05-24 14:08:03 -0700

    add particles & new favicon

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, abort
+from flask_bootstrap import Bootstrap5
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField, SelectField
+from wtforms.validators import DataRequired
+import smtplib
+import os
+import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
